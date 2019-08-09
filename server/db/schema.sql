372b2b0bf7d71fbb2565d294ef3efd87fc661af0
Jennie Zeng <jenniezeng1@gmail.com>
2019-08-09 12:04:37 -0700

    initial pg connection and schema set up

---
+++ server/db/schema.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS cooking_time;
+CREATE DATABASE cooking_time;
+\connect cooking_time;
+
+DROP TABLE IF EXISTS recipes;
+DROP TABLE IF EXISTS reviews;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE IF NOT EXISTS users (
+  id serial PRIMARY KEY,
+  user_name varchar(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS recipes (
+  id serial PRIMARY KEY,
+  recipe_name varchar(100) NOT NULL,
+  user_id serial REFERENCES users(id),
+  ingredients text NOT NULL,
+  directions text NOT NULL,
+  serving_size int DEFAULT 2,
+  cooking_time int DEFAULT 30,
+  images text[]
+);
+
+CREATE TABLE IF NOT EXISTS reviews (
+  id serial PRIMARY KEY,
+  review_text text NOT NULL,
+  user_id serial REFERENCES users(id),
+  recipe_id REFERENCES recipes(id)
+);
