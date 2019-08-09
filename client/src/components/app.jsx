886f561aea8338fc84a1b75e5b9c4270ae63c35f
Jennie Zeng <jenniezeng1@gmail.com>
2019-08-09 16:10:16 -0700

    index and appjsx updated

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import CreateRecipe from './CreateRecipe';
 
 class App extends React.Component {
-	constructor() {
-		super();
-	}
+  constructor() {
+    super();
+  }
 
-	render() {
-		return (
-			<div></div>
-		)
-	}
+  render() {
+    return (
+      <div>
+        <CreateRecipe />
+      </div>
+    )
+  }
 }
+
+export default App;
