d12255863e4a08049a48d0d2c89789e11244b33a
Jennie Zeng <jenniezeng1@gmail.com>
2019-08-11 10:12:54 -0700

    return url for uploading multiple files

--- server/Model/uploadImage.js
+++ server/Model/uploadImage.js
@@ -1,10 +1,30 @@
 const upload = require('../services/s3ImgUpload');
 
-const singleUpload = upload.single('recipePic');
+// const singleUpload = upload.single('recipePic');
+
+// module.exports = (req, res) => {
+//   singleUpload(req, res, (err) => {
+//     if (err) {
+//       console.log(err);
+//       return res.status(422).send({
+//         errors: [{
+//           title: 'File Upload Error',
+//           detail: err.message,
+//         }],
+//       });
+//     }
+
+//     return res.status(201).json({ 'imageUrl': req.file.location });
+//   });
+// };
+
+
+const multipleUpload = upload.array('recipePic');
 
 module.exports = (req, res) => {
-  singleUpload(req, res, (err) => {
+  multipleUpload(req, res, (err) => {
     if (err) {
+      console.log(err);
       return res.status(422).send({
         errors: [{
           title: 'File Upload Error',
@@ -12,7 +32,14 @@ module.exports = (req, res) => {
         }],
       });
     }
+    
+    const filesLocation = [];
 
-    return res.json({ 'imageUrl': req.file.location });
+    for (let i = 0; i < req.files.length; i ++) {
+      console.log(req.files[i].location)
+      filesLocation.push(req.files[i].location);
+    }
+    
+    return res.status(201).json({ 'imageUrl': filesLocation });
   });
 };
