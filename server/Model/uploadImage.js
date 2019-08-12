639f3b73bea87c96761fa7726c71b360536a166d
Jennie Zeng <jenniezeng1@gmail.com>
2019-08-11 23:27:33 -0700

    code clean up

--- server/Model/uploadImage.js
+++ server/Model/uploadImage.js
@@ -1,24 +1,5 @@
 const upload = require('../services/s3ImgUpload');
 
-// const singleUpload = upload.single('recipePic');
-
-// module.exports = (req, res) => {
-//   singleUpload(req, res, (err) => {
-//     if (err) {
-//       console.log(err);
-//       return res.status(422).send({
-//         errors: [{
-//           title: 'File Upload Error',
-//           detail: err.message,
-//         }],
-//       });
-//     }
-
-//     return res.status(201).json({ 'imageUrl': req.file.location });
-//   });
-// };
-
-
 const multipleUpload = upload.array('recipePic');
 
 module.exports = (req, res) => {
